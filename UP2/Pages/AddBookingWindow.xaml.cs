aac09cb61213ead3790c23984355a05e44b6c307
Ilya <ilyanosevich@gmail.com>
2025-01-26 16:03:04 +0300

    Добавьте файлы проекта.

---
+++ UP2/Pages/AddBookingWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UP2.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddBookingWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class AddBookingWindow : Window
+    {
+        Booking _currentbooking;
+        HotelEntities HE = HotelEntities.GetContext();
+
+        public event Action<Booking> OnBookingAdded;
+
+        public AddBookingWindow()
+        {
+            _currentbooking = new Booking();
+            Initialize();
+        }
+
+        public AddBookingWindow(Booking booking)
+        {
+            _currentbooking = booking;
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            InitializeComponent();
+            _currentbooking.CheckInDate = DateTime.Now;
+            _currentbooking.CheckOutDate = DateTime.Now;
+            DataContext = _currentbooking;
+            var List = HE.Clients.ToList();
+            List.Reverse();
+            ClientsList.ItemsSource = List;
+            RoomsList.ItemsSource = HE.Rooms.ToList();
+            
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (ClientsList.SelectedIndex == -1 ||
+                RoomsList.SelectedIndex == -1 ||
+                CheckInDatePicker.SelectedDate < DateTime.Now||
+                CheckOutDatePicker.SelectedDate <= CheckInDatePicker.SelectedDate)
+            {
+                MessageBox.Show("Неверный формат даты или не заполнены все поля");
+                return;
+            }
+            HE.Bookings.Add(_currentbooking);
+            HE.SaveChanges();
+            MessageBox.Show("Информация сохранена!");
+            OnBookingAdded.Invoke(_currentbooking);
+            Close();
+        }
+    }
+}
---
+++ UP2/Pages/BookingPage.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace UP2.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для BookingPage.xaml
+    /// </summary>
+    public partial class BookingPage : Page
+    {
+
+        AddBookingWindow addBookingWindow;
+        
+        HotelEntities HE = HotelEntities.GetContext();
+        public BookingPage()
+        {
+            InitializeComponent();
+            DGridBookings.ItemsSource = FullBookingsList();
+        }
+
+        private List<Booking> FullBookingsList()
+        {
+            var List = HE.Bookings.ToList();
+            List.Reverse();
+            return List;
+        }
+
+        private void ShowAll_Click(object sender, RoutedEventArgs e)
+        {
+            DGridBookings.ItemsSource = FullBookingsList();
+            BeginFilterDateText.SelectedDate = null;
+            EndFilterDateText.SelectedDate = null;
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            addBookingWindow = new AddBookingWindow();
+            addBookingWindow.Show();
+             
+            addBookingWindow.OnBookingAdded += (newbooking) =>
+            {
+                DGridBookings.ItemsSource = FullBookingsList();
+            };
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItems = DGridBookings.SelectedItems.Cast<Booking>().ToList();
+            if (selectedItems.Any())
+            {
+                foreach (var item in selectedItems.Select(itm => itm))
+                {
+                    HE.Bookings.Remove(item);
+                }
+                HE.SaveChanges();
+                DGridBookings.ItemsSource = FullBookingsList();
+            }
+        }
+
+        private void Filter(DateTime FilterDate, bool isBegin)
+        {
+            List<Booking> BookingList = new List<Booking>();
+
+            if (isBegin)
+            {
+                foreach (Booking booking in HE.Bookings.ToList())
+                {
+                    DateTime checkindate = booking.CheckInDate;
+                    if (checkindate >= FilterDate)
+                    {
+                        BookingList.Add(booking);
+                    }
+                }
+            }
+            else
+            {
+                foreach (Booking booking in HE.Bookings.ToList())
+                {
+                    DateTime checkindate = booking.CheckInDate;
+                    if (checkindate <= FilterDate)
+                    {
+                        BookingList.Add(booking);
+                    }
+                }
+            }
+            BookingList.Reverse();
+            DGridBookings.ItemsSource = BookingList;
+        }
+        private void Filter(DateTime BeginDate, DateTime EndDate)
+        {
+            List<Booking> BookingList  = new List<Booking>();
+            foreach (Booking booking in HE.Bookings.ToList())
+            {
+                DateTime checkindate = booking.CheckInDate;
+                if (checkindate >= BeginDate && checkindate < EndDate)
+                {
+                    BookingList.Add(booking);
+                }
+            }
+            BookingList.Reverse();
+            DGridBookings.ItemsSource = BookingList;
+        }
+
+        private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string BeginDateText = BeginFilterDateText.Text;
+            string EndDateText = EndFilterDateText.Text;
+
+            DateTime BeginDate = new DateTime();
+            DateTime EndDate = new DateTime();
+            if (DateTime.TryParse(BeginDateText, out BeginDate) && DateTime.TryParse(EndDateText, out EndDate))
+            {
+                Filter(BeginDate, EndDate);
+            }
+            else if (DateTime.TryParse(BeginDateText, out BeginDate))
+            {
+                Filter(BeginDate, true);
+            }
+            else if (DateTime.TryParse(EndDateText, out EndDate))
+            {
+                Filter(EndDate, false);
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(BeginDateText) || !string.IsNullOrEmpty(EndDateText))
+                {
+                    MessageBox.Show("Неверный формат даты");
+                }
+                DGridBookings.ItemsSource = FullBookingsList();
+            }
+        }
+    }
+}
